79952f6393c9c90e4afa4fd770f41a7da73a0632
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-03-26 18:05:39 +0100

    feat: pop

--- src/main/scala/it/unibo/pps/tasks/adts/Ex3Stack.scala
+++ src/main/scala/it/unibo/pps/tasks/adts/Ex3Stack.scala
@@ -15,7 +15,9 @@ object Ex3Stacks:
 
   trait StackADT:
     type Stack[A]
+
     def empty[A]: Stack[A] // factory
+
     extension [A](stack: Stack[A])
       def push(a: A): Stack[A]
       def pop(): Optional[(A, Stack[A])]
@@ -23,8 +25,14 @@ object Ex3Stacks:
 
   object StackImpl extends StackADT:
     opaque type Stack[A] = Sequence[A]
+
     def empty[A]: Stack[A] = Nil()
+
     extension [A](stack: Stack[A])
       def push(a: A): Stack[A] = Cons(a, stack)
-      def pop(): Optional[(A, Stack[A])] = ???
+
+      def pop(): Optional[(A, Stack[A])] = stack match
+        case Cons(h, t) => Optional.Just((h, t))
+        case _ => Optional.None()
+
       def asSequence(): Sequence[A] = stack
